5aff1769469575645405ae299a1aea1164e6a4a2
Thiago Souza <thlima.silva.souza@gmail.com>
2021-01-19 10:15:31 -0300

    criando conexao com o DB

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+
+
+//View engine
+app.set('view engine','ejs');
+
+//Body parser
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//Database
+connection
+.authenticate()
+.then(() => {
+    console.log("Conexão feita com sucesso!");
+}).catch((error) => {
+    console.log(error);
+})
+
+//Static
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-    res.send("Bem vindo ao meu site");
+    res.render("index");
 
 })
 
