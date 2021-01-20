7c9e205e9e55770d421a80c49621f6a7d85cc3d9
Thiago Souza <thlima.silva.souza@gmail.com>
2021-01-20 17:32:24 -0300

    Criando rotas para as Categorias e os Artigos

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
 
+const categoriesController = require("./categories/CategoriesController");
+const articlesController = require("./articles/ArticlesController");
+
 
 //View engine
 app.set('view engine','ejs');
@@ -19,6 +22,10 @@ connection
 }).catch((error) => {
     console.log(error);
 })
+
+
+app.use("/", categoriesController);
+app.use("/", articlesController);
 
 //Static
 app.use(express.static('public'));
