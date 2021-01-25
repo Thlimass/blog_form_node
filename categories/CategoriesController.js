f0245cef14f797ede0cfa048e7e9bcee0fe152a0
Thiago Souza <thlima.silva.souza@gmail.com>
2021-01-25 18:26:17 -0300

    Cadastro de categorias e Slugify

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const Categoty = require("./Category");
+const slugify = require("slugify")
 
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new")
 });
 
-module.exports = router;
+router.post("/categories/save", (req, res) => {
+    var title = req.body.title;
+    if(title != undefined || title != ""){
+
+        Categoty.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect("/");
+        })
+    
+    }else{
+        res.redirect("admin/categories/new")
+    }
+
+})
+
+module.exports = router;    
