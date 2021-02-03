a95c4fe4df99befe0e77e2ba0f32b51e620eda75
Thiago Souza <thlima.silva.souza@gmail.com>
2021-02-03 18:09:39 -0300

    Deletando categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Categoty = require("./Category");
+const Category = require("./Category");
 const slugify = require("slugify")
 
 router.get("/admin/categories/new", (req, res) => {
@@ -11,7 +11,7 @@ router.post("/categories/save", (req, res) => {
     var title = req.body.title;
     if(title != undefined || title != ""){
 
-        Categoty.create({
+        Category.create({
             title: title,
             slug: slugify(title)
         }).then(() => {
@@ -26,10 +26,29 @@ router.post("/categories/save", (req, res) => {
 
 router.get("/admin/categories", (req, res) => {
 
-    Categoty.findAll().then(categories => {
+    Category.findAll().then(categories => {
         res.render("admin/categories/index", {categories: categories});
     });
        
 });
+
+router.post("/categories/delete", (req,res) => {
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){ 
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories")
+            });
+        }else{// NÃO FOR UM NÚMERO
+            res.redirect("/admin/categories")
+        }
+    }else{// NULL
+        res.redirect("/admin/categories");
+    }
+})
 
 module.exports = router;    
