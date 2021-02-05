cbf0992112b704275e1ea5c2922f216f38bcacf4
Thiago Souza <thlima.silva.souza@gmail.com>
2021-02-05 18:59:50 -0300

    Salvando artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const Category = require("../categories/Category")
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
 router.get("/articles", (req, res) => {
     res. send("Rota de Artigos")
@@ -11,6 +13,19 @@ router.get("/admin/articles/new", (req, res) => {
         res.render("admin/articles/new", {categories: categories})
     })
     
+});
+
+router.post("/articles/save", (req,res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        cotegoryId: category
+    })
 });
 
 module.exports = router;
